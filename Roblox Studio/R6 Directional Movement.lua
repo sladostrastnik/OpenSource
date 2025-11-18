437cec219e48ba0995bea84f32d36577f4a68d47
Slado <138035459+sladostrastnik@users.noreply.github.com>
2025-11-18 18:35:29 +0300

    Update R6 Directional Movement.lua

--- Roblox Studio/R6 Directional Movement.lua
+++ Roblox Studio/R6 Directional Movement.lua
@@ -1,6 +1,4 @@
-local TorsoXRotation = 10
-local TorsoZRotation = 15
-local LegRotation = 10
+local Time, TorsoXRotation, TorsoZRotation, LegRotation = 0.25, 5, 15, 10
 
 local RunService = game:GetService("RunService")
 local TweenService = game:GetService("TweenService")
@@ -10,33 +8,34 @@ local Player = Players.LocalPlayer
 local Character = script.Parent
 local Humanoid = Character:WaitForChild("Humanoid")
 
-local HumanoidRootPart = Character.HumanoidRootPart
-local Torso = Character.Torso
-local RootJoint = HumanoidRootPart.RootJoint
-local LeftHipJoint = Torso["Left Hip"]
-local RightHipJoint = Torso["Right Hip"]
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+local Torso = Character:WaitForChild("Torso")
+local RootJoint = HumanoidRootPart:WaitForChild("RootJoint")
+local LeftHipJoint = Torso:WaitForChild("Left Hip")
+local RightHipJoint = Torso:WaitForChild("Right Hip")
 
 local Force = nil
 local Direction = nil
-local Value1 = 0
-local Value2 = 0
+local RightDot = 0
+local LookDot = 0
 
 local RootJointC0 = RootJoint.C0
 local LeftHipJointC0 = LeftHipJoint.C0
 local RightHipJointC0 = RightHipJoint.C0
 
+local TI =  TweenInfo.new(Time, Enum.EasingStyle.Sine)
 RunService.RenderStepped:Connect(function()
 	Force = HumanoidRootPart.Velocity * Vector3.new(1, 0, 1)
 	if Force.Magnitude > 0.001 then
 		Direction = Force.Unit
-		Value1 = HumanoidRootPart.CFrame.RightVector:Dot(Direction)
-		Value2 = HumanoidRootPart.CFrame.LookVector:Dot(Direction)
+		RightDot = HumanoidRootPart.CFrame.RightVector:Dot(Direction)
+		LookDot = HumanoidRootPart.CFrame.LookVector:Dot(Direction)
 	else
-		Value1 = 0
-		Value2 = 0
+		RightDot = 0
+		LookDot = 0
 	end
-	
-	TweenService:Create(RootJoint, TweenInfo.new(0.25, Enum.EasingStyle.Sine), {C0 = RootJoint.C0:Lerp(RootJointC0 * CFrame.Angles(math.rad(Value2 * TorsoZRotation), math.rad(-Value1 * TorsoXRotation), 0), 0.5)}):Play()
-	TweenService:Create(LeftHipJoint, TweenInfo.new(0.25, Enum.EasingStyle.Sine), {C0 = LeftHipJoint.C0:Lerp(LeftHipJointC0 * CFrame.Angles(math.rad(Value1 * LegRotation), 0, 0), 0.5)}):Play()
-	TweenService:Create(RightHipJoint, TweenInfo.new(0.25, Enum.EasingStyle.Sine), {C0 = RightHipJoint.C0:Lerp(RightHipJointC0 * CFrame.Angles(math.rad(-Value1 * LegRotation), 0, 0), 0.5)}):Play()
+
+	TweenService:Create(RootJoint, TI, {C0 = RootJoint.C0:Lerp(RootJointC0 * CFrame.Angles(math.rad(LookDot * TorsoZRotation), math.rad(-RightDot * TorsoXRotation), 0), 0.5)}):Play()
+	TweenService:Create(LeftHipJoint, TI, {C0 = LeftHipJoint.C0:Lerp(LeftHipJointC0 * CFrame.Angles(math.rad(RightDot * LegRotation), 0, 0), 0.5)}):Play()
+	TweenService:Create(RightHipJoint, TI, {C0 = RightHipJoint.C0:Lerp(RightHipJointC0 * CFrame.Angles(math.rad(-RightDot * LegRotation), 0, 0), 0.5)}):Play()
 end)
